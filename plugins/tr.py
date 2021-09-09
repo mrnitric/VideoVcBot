1261a449540ddc67b9df3f99663326cd12dd8639
mrnitric <80440353+mrnitric@users.noreply.github.com>
2021-09-09 07:29:16 +0530

    Add files via upload

---
+++ plugins/tr.py
@@ -0,0 +1,7 @@
+import os
+from config import ASSISTANT_NAME
+
+HELP_TEXT =f"**Sᴇᴛᴛɪɴɢs** \n\n`1) Aᴅᴅ Tʜɪs Bᴏᴛ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Mᴀᴋᴇ Iᴛ Aᴅᴍɪɴ` \n2) **Aᴅᴅ** @{ASSISTANT_NAME} **Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ** \n\n**Cᴏᴍᴍᴀɴᴅs** \n\n- `/stream` **: Rᴇᴘʟʏ Tᴏ Vɪᴅᴇᴏ Oʀ Fɪʟᴇ Tʜᴀᴛ Yᴏᴜ Wᴀɴᴛ Tᴏ Sᴛʀᴇᴀᴍ Iɴ Vᴄ.** \n- `/stopstream`  **: Sᴛᴏᴘ Tʜᴇ Sᴛʀᴇᴀᴍ**"
+START_TEXT = '👋 `Hᴇʏ Mʏ Nᴀᴍᴇ Is` **⫷ 𝙑𝙞𝙙𝙚𝙤 𝙋𝙡𝙖𝙮𝙚𝙧 ⫸**. \n\n`Lᴇᴛs Eɴᴊᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ  Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs ❤️😊`'
+ABOUT_TEXT = '**👩‍💻 Iɴғᴏʀᴍᴀᴛɪᴏɴ** \n\n`🤖 Tʜɪs Bᴏᴛ Wᴀs Cʀᴇᴀᴛᴇᴅ Tᴏ Sᴛʀᴇᴀᴍ Vɪᴅᴇᴏ Iɴ Tᴇʟᴇɢʀᴀᴍ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Cʜᴀᴛs Usɪɴɢ Sᴇᴠᴇʀᴀʟ Mᴇᴛʜᴏᴅs Fʀᴏᴍ Wᴇʙʀᴛᴄ.` \n\n`💡 Pᴏᴡᴇʀᴇᴅ Bʏ :- @Sanki_BOTs '
+DEVS_TEXT =f"**Cʀᴇᴅɪᴛ** :- @Oo_Elrix 💚🍹. "
